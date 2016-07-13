fd39daca2e4a881674b11bcc03e29435d137feb0
RebeccaGray <rebecca.gray@gmail.com>
2016-07-12 22:43:13 -0400

    process survey results, getuserStatus stores user info if new user, construct matches and serve up to front end

--- app/server/userController.js
+++ app/server/userController.js
@@ -8,8 +8,28 @@ var db = require ('./server.js')
 
 module.exports = {
   addTestData: function(currentUser, deck, testResults, cb){
+
     //create compareArray & add it to data before inserting it
+
     var compareArray = []
+
+    //fixed order for personality_type results
+    var fixedOrderOfResultsArray =
+    [
+    'Adventurous', 'Reliable', 'Charismatic', 'Mellow', 'Rational', 'Thoughtful', 'Social',
+    ]
+    // pull compare data from test object
+    var  res = []
+    for (var i=0;i<personality_types.length;i++){
+      item = personality_types[i]
+      res.push(
+      {
+        personality_type:item.personality_type.name,
+        score:item.score })
+    }
+    //[{personality_type:name,score:score}]
+
+    compareArray.push(res)
 
     //to currentUser
       db.User.findByIdAndUpdate(currentUser._id, {
@@ -24,8 +44,10 @@ module.exports = {
     function(err,res){
         if (err){
           console.error(err)
+          cb(false)
         } else {
-          cb('test results added')
+          console.log('testResults and compare array added')
+          cb(true)
         }
       })
   },
@@ -43,37 +65,77 @@ module.exports = {
     function(err,res){
         if (err){
           console.error(err)
+          cb(false)
         } else {
-          cb('test questions added')
+          cb(true)
         }
       })
   },
-  signup: function(facebookObject){
+  signup: function(facebookObject, cb){
     var user = new User({
      'facebookObject': facebookObject
    })
-   // var user = new User({'name':'Mary'})
     user.save(function(err,user){
-      if(err) return console.error(err);
-    })
+      if(err) return console.error('user signup ' + err);
+    } else {
+      cb({'newUser': user})
+    }
+   })
   },
   queryMatches(currentUser,deck,cb){
     var currentUserScores = currentUser.testObject.deck.compareArray;
-    //forEach user in Users
 
       db.User.find().all(function(user){
+        //forEach user in Users generate differences array with currentUser
 
-      })
+        compareArray = user.testObject.deck.compareArray;
+        var greatestDifference=0, difference, matches = [];
 
+        //find greatest difference between user scores
+
+        for var(i=0;i<compareArray.length;i++){
+          difference = Math.abs(compareArray[i] - currentUserScores[i])
+          if (difference > greatestDifference){
+            greatestDifference = difference;
+          }
+         }
+
+         //add each user's profile and their difference score to the matches object
+
+         matches.push(
+           {
+             greatestDifference: greatestDifference, facebookObject: user.facebookObject
+           })
+        }
+
+        //sort the matches objet and return
+        var resultsArray = _.orderBy(matches, ['greatestDifference', 'facebookObject'], ['desc'])
+        // returns → objects for [[36, fbobj], [34, fbobj]]
+
+        //return  {currentUser:{fbObj},matches: [ fbObj , fbObj , fbObj ] }
+        var matchesObjet =
+        {
+          'currentUser': user.facebookObject,
+          'matches': null
+        }
+        matches = []
+        for (var i=0;i<resultsArray.length;i++){
+          matches.push(resultsArray[i][1])
+        }
+        cb(matchesObject)
 
     //test user test has results
-       //if has results, test scoreArray with fuzzy match
-       //add to matchesObject in order of scores difference
-       //may need intermediate object with keys of difference
-
+       //if has results, compute difference between current user
+       //score for each test and each user in db
+       //add to matchesObject in order of scores difference <
+       //the order of the results for comparison is fixed in the
+       //FixedOrderOfResultsArray for the core deck test.
+       //if additional tests are added, this will need to be developed further.
   },
-  getUserStatus: function(facebookId, cb){
-    //if new user return {'newUser':null}
+  getUserStatus: function(facebookId, facebookObject, cb){
+    //this is the user routing function
+
+    //if new user add to db & cb{'newUser':null}
     //if existing user no survey data cb {'existingUserUnfinshedSurvey':null}
     //if existing user with survey data
     //>>query db for matches
@@ -85,12 +147,17 @@ module.exports = {
      },
      function(err,currentUser){
      if (err){
-       cb({'newUser':null})
+       //newUser because not in db
+       //proced to signup if new User
+       exports.signup(facebookObject, cb)
+
      }
      if (currentUser.tObj.testResults.length > 0){
+
        //existingUserSurveyComplete
        //get matches query results
        //deck is variable.. setting to core default
+
        if (!deck){
          var deck = 'core';
        }
@@ -112,7 +179,8 @@ module.exports = {
 
 //this is the data structure
 
-// user1{_id:mongoID,fbObj:fObj,tObj:tObj}
+// user = {_id:mongoID,fbObj:fObj,tObj:tObj}
+
 // {
 //   _id:mongoId,
 
