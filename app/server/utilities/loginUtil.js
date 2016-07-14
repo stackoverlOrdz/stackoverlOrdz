ec18fe80d3658f60aab80079918558bdddc5391a
RebeccaGray <rebecca.gray@gmail.com>
2016-07-14 17:33:27 -0400

    modified callbacks to front end

--- app/server/utilities/loginUtil.js
+++ app/server/utilities/loginUtil.js
@@ -3,26 +3,31 @@ var traitifyAPICalls = require('./traitifyUtils/traitifyAPICalls.js');
 
 
 exports.routeUser = function(facebookData, callback) {
-  // userController.getUserStatus(facebookData.id, facebookData, function(response) {
+   userController.getUserStatus(facebookData.id, facebookData, function(response) {
   returnNewUser(function(response) {
-    console.log("response", response);
+    console.log("+++ line 8routeUser", response);
     if (response.newUser) {
+      var responseObject = response.newUser
       // console.log("new user! :", response);
       // create new 'core' survey and then retrieve it
       traitifyAPICalls.createAssessment('core', function(surveyInfo) {
         traitifyAPICalls.getAssessment(JSON.parse(surveyInfo).id, function(survey) {
           // TO-DO: get userData instead of facebookData when getUserStatus fixed
-          callback("survey", survey, facebookData);
+          responseObject.data = survey
+          //{route:survey,data:survey,currentUser:userObject}
+          callback(responseObject);
         });
       });
     } else if (response.existingUserSurveyComplete) {
+      var responseObject = response.existingUserSurveyComplete
+      //{route:matches,data:matches,currentUser:userObject}
       console.log("existing user, survey complete");
-      // get "matches from DB"
-      // callback("matches", matches, facebookData);
+      callback(responseObject);
     } else {
       callback("fail");
     }
   });
+})
 }
 
 
