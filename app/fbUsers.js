087b50584660c81cc5cbb377e02e75dc222f1dec
Deniz Tetik <deniztetik31@gmail.com>
2016-07-15 15:17:27 -0400

    function to create sample users

--- app/fbUsers.js
+++ app/fbUsers.js
@@ -1,3 +1,6 @@
+var _ = require('lodash');
+
+// SAMPLE
 // id: '4446615048021',
 //   name: 'Deniz Aslan',
 //   picture:
@@ -10,18 +13,35 @@
 //   location: { id: '108424279189115', name: 'New York, New York' },
 //   verified: true }
 
+// TODO: add random pictures for people, add to db
+
 var firstNames = ['Joe', 'Andrew', 'Adam', 'Eve', 'Barbara', 'George', 'Frodo', 'Bohee', 'Rebecca', 'Deniz', 'Joseph', 'John', 'Tyler', 'Phil', 'Abigail', 'Anteo', 'Alex'];
 var lastNames = ['Shmo', 'Leonardi', 'Adams', 'Reiser', 'Jennings', 'Taylor', 'Baggins', 'Park', 'Gray', 'Tetik', 'Martin', 'Michelin', 'Lambe', 'Teves', 'Felix', 'Fabris', 'Friedman'];
 
 var pictures = [];
 var emailHandles = ['coolkid196', 'lamepersonhater183', 'xofirefairyxo', 'newyorkkid482', 'californiagurl86', 'emperorpenguin23', 'temporaryperson389', 'preppydude283', 'lambosrkool39', 'salsalady49', 'iliketurtles01', 'warmblankets33', 'billabongguy38', 'guitarist942', 'pumalover91', 'solitaryperson129', 'audiodork0871', 'sailor927', 'cooltrainerjoey'];
 var emailDomains = ['yahoo', 'gmail', 'me', 'icloud'];
-// var months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var birthYears = ['78', '80', '82', '84', '86', '88', '90', '92', '94'];
 
 var fbLink = 'https://example.com';
-var cities = [ 'New York, New York' ];
+var cities = 'New York, New York';
 var cityId = '108424279189115';
 
 function populateTestUsers(numberOfUsers) {
-  return [];
+  var userEmailHandles = emailHandles;
+  console.log(emailHandles);
+  var testUsers = [];
+  for (var i = 0; i < numberOfUsers; i++) {
+    var testUser = {};
+    testUser.name = _.sample(firstNames) + ' ' + _.sample(lastNames);
+    testUser.picture = { data: { is_silhouette: false, url: 'https://example.com' } };
+    testUser.email = userEmailHandles.pop() + '@' + _.sample(emailDomains) + '.com';
+    testUser.birthday = '1/1/' + _.sample(birthYears);
+    testUser.location = { id: cityId, name: cities };
+    testUser.verified = true;
+    testUsers.push(testUser);
+  }
+  return testUsers;
 }
+
+console.log(populateTestUsers(18  ));
